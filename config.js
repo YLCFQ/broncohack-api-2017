1e42272c14d5429fc0793e6506ec35679a6e8c65
YLCFQ <chen41384406@gmail.com>
2017-04-15 03:59:19 -0700

    fix static folder

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   'secret' : 'BroncoHack2017',
   'site' : 'http://localhost:8080/',
-  'static_folder' : '/usr/local/var/www/static/',
+  'static_folder' : '/usr/share/nginx/html/static/',
   'default_picture' : 'http://localhost:8080/static/default.png',
   'default_item_picture' : ['http://localhost:8080/static/default_item.png'],
   'token_expire' : 60 * 60 * 24 * 7,
